b9d2f353f9f60e52441254add02a3a81e0c25e6d
KENT(けんT) <52515974+4456-kent@users.noreply.github.com>
2019-11-25 21:50:13 +0900

    Update test_MPU6050.py
    とりあえず動く保証ないけど書いた

--- プログラムのソースコード/MPU6050/test_MPU6050.py
+++ プログラムのソースコード/MPU6050/test_MPU6050.py
@@ -1,4 +1,18 @@
+import wiringpi as pi
+import time
+import mpu6050
 
+mpu6050_addr=0x68
 
+pi.wiringPiSetupGpio()
+i2c=pi.I2C()
+acc=mpu6050.mpu6050(i2c,mpu6050_addr)
+acc.set_accel_range()
+acc.set_gyro_range()
 
-
+while True:
+    (acc_x,acc_y,acc_z)=acc.get_accel_data()
+    (gyro_x,gyro_y,gyro_z)=acc.get_gyro_data()
+    print("ACC_X:",acc_x,"ACC_Y:",acc_y,"ACC_Z:",acc_z,"\n")
+    print("GYRO_X:",gyro_x,"GYRO_Y:",gyro_y,"GYRO_Z:",gyro_z,"\n")
+    time.sleep(1)
