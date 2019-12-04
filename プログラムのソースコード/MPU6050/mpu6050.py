2a1bddf7a9aac51b904b7876449029cef3acdc7a
KENT(けんT) <52515974+4456-kent@users.noreply.github.com>
2019-12-04 22:39:13 +0900

    Update mpu6050.py
    acc gyro両方からMODIFIERを除去(一旦)

--- プログラムのソースコード/MPU6050/mpu6050.py
+++ プログラムのソースコード/MPU6050/mpu6050.py
@@ -15,7 +15,7 @@ class mpu6050:
 
     def get_acc_value(self):
         buf=[]
-        ACCEL_SCALE_MODIFIER=8192.0 #加速度のフルスケールを±4Gに設定したときの値
+        
         i=0
         while(i<6):
              buf.append(self.i2c.readReg8(self.mpu6050,0x3b+i))
@@ -25,15 +25,15 @@ class mpu6050:
         acc_y_mesure=((buf[2] << 8) | buf[3])
         acc_z_mesure=((buf[4] << 8) | buf[5])
 
-        acc_x=acc_x_mesure/ACCEL_SCALE_MODIFIER
-        acc_y=acc_y_mesure/ACCEL_SCALE_MODIFIER
-        acc_z=acc_z_mesure/ACCEL_SCALE_MODIFIER
+        acc_x=acc_x_mesure
+        acc_y=acc_y_mesure
+        acc_z=acc_z_mesure
         
         return(acc_x,acc_y,acc_z)
         
     def get_gyro_value(self):
         buf=[]
-        GYRO_SCALE_MODIFIER=65.5    #角速度のフルスケールを±500deg/sに設定したときの値
+        
         i=0
         while(i<6):
              buf.append(self.i2c.readReg8(self.mpu6050,0x43+i))
@@ -43,9 +43,9 @@ class mpu6050:
         gyro_y_mesure=((buf[2] << 8) | buf[3])
         gyro_z_mesure=((buf[4] << 8) | buf[5])
 
-        gyro_x=gyro_x_mesure/GYRO_SCALE_MODIFIER
-        gyro_y=gyro_y_mesure/GYRO_SCALE_MODIFIER
-        gyro_z=gyro_z_mesure/GYRO_SCALE_MODIFIER
+        gyro_x=gyro_x_mesure
+        gyro_y=gyro_y_mesure
+        gyro_z=gyro_z_mesure
 
         return(gyro_x,gyro_y,gyro_z)
 
