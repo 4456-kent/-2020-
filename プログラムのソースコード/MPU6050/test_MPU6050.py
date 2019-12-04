9987fa891df2de2c471b728516d3bb0c9b51c998
KENT(けんT) <52515974+4456-kent@users.noreply.github.com>
2019-12-04 18:28:55 +0900

    move test_mpu6050.py

--- プログラムのソースコード/MPU6050/test_MPU6050.py
+++ プログラムのソースコード/MPU6050/test_MPU6050.py
@@ -2,17 +2,19 @@ import wiringpi as pi
 import time
 import mpu6050
 
+SAMPLING_RATE=10    #[Hz]
+
 mpu6050_addr=0x68
 
 pi.wiringPiSetupGpio()
+
 i2c=pi.I2C()
-acc=mpu6050.mpu6050(i2c,mpu6050_addr)
-acc.set_accel_range()
-acc.set_gyro_range()
+acceleration=mpu6050.mpu6050(i2c,mpu6050_addr)
+acceleration.setup()
 
 while True:
-    (acc_x,acc_y,acc_z)=acc.get_accel_data()
-    (gyro_x,gyro_y,gyro_z)=acc.get_gyro_data()
-    print("ACC_X:",acc_x,"ACC_Y:",acc_y,"ACC_Z:",acc_z,"\n")
-    print("GYRO_X:",gyro_x,"GYRO_Y:",gyro_y,"GYRO_Z:",gyro_z,"\n")
-    time.sleep(1)
+    (acc_x,acc_y,acc_z,temp,gyro_x,gyro_y,gyro_z)=acceleration.get_value()
+
+    print(acc_x,acc_y,acc_z,temp,gyro_x,gyro_y,gyro_z)
+    time.sleep(1/SAMPLING_RATE)
+
