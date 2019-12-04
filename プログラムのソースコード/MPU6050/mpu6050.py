1c0f4ff53707a9382d76abda7cb5124ef2d75754
KENT(けんT) <52515974+4456-kent@users.noreply.github.com>
2019-12-05 00:07:54 +0900

    Changes test_MPU6050.py and mpu6050.py
    気温、角速度を追加
    コメント追加

--- プログラムのソースコード/MPU6050/mpu6050.py
+++ プログラムのソースコード/MPU6050/mpu6050.py
@@ -1,23 +1,31 @@
+"""
+
+本ファイルはMPU6050のpythonのlib
+データシート：https://www.invensense.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf
+アドレスマップ：https://www.invensense.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
+
+"""
+
 import wiringpi as pi
 
 class mpu6050:
  
-    def __init__(self,i2c,addr):
+    def __init__(self,i2c,addr):       #I2Cの初期化
         self.addr=addr
         self.i2c=i2c
 
         self.mpu6050=self.i2c.setup(self.addr)
 
-    def setup(self):
-        self.i2c.writeReg8(self.mpu6050,0x6b,0x01)
-        self.i2c.writeReg8(self.mpu6050,0x1c,0x08)
-        self.i2c.writeReg8(self.mpu6050,0x1b,0x08)
-        self.i2c.writeReg8(self.mpu6050,0x1a,0x02)
+    def setup(self):    #最初のI2Cの書き込み
+        self.i2c.writeReg8(self.mpu6050,0x6b,0x01)  #Bit6のSLEEPをLOWにしないと値が0しか返ってこなくなる
+        self.i2c.writeReg8(self.mpu6050,0x1c,0x08)  #Full Scale Rangeを±4Gに設定
+        self.i2c.writeReg8(self.mpu6050,0x1b,0x08)  #Full Scale Rangeを±500deg/sに設定
+        self.i2c.writeReg8(self.mpu6050,0x1a,0x02)  #フィルターを約96Hzに設定
 
 
-    def get_acc_value(self):
+    def get_acc_value(self):    #加速度の値取得
         buf=[]
-        ACCEL_SCALE_MODIFIER=8192.0
+        ACCEL_SCALE_MODIFIER=8192.0 #Full Scale Rangeを±4Gに設定したときの値
         i=0
         while(i<6):
              buf.append(self.i2c.readReg8(self.mpu6050,0x3b+i))
@@ -32,10 +40,23 @@ class mpu6050:
         acc_z=acc_z_mesure/ACCEL_SCALE_MODIFIER
         
         return(acc_x,acc_y,acc_z)
-        
-    def get_gyro_value(self):
+
+    def get_temp_value(self):   #気温の値を取得
         buf=[]
-        GYRO_SCALE_MODIFIRE=65.5
+        i=0
+        while(i<2):
+            buf.append(self.i2c.readReg8(self.mpu6050,0x41+1))
+            i=i+1
+
+        temp_mesure=((buf[0] << 8) | buf[1])
+
+        temp=temmp_mesure/340+36.53
+
+        return(temp)
+        
+    def get_gyro_value(self):   #角速度の値を取得
+        buf=[]
+        GYRO_SCALE_MODIFIRE=65.5    ##Full Scale Rangeを±500deg/sに設定したときの値
         i=0
         while(i<6):
              buf.append(self.i2c.readReg8(self.mpu6050,0x43+i))
--- プログラムのソースコード/MPU6050/test_MPU6050.py
+++ プログラムのソースコード/MPU6050/test_MPU6050.py
@@ -2,8 +2,8 @@ import wiringpi as pi
 import time
 import mpu6050
 
-mpu6050_addr=0x68
-SAMPLING_RATE=10
+mpu6050_addr=0x68   #Raspberry Pi側のコンソール上で"i2cdetect 1"コマンドで確認
+SAMPLING_RATE=10    #サンプリングレート[Hz]
 
 pi.wiringPiSetupGpio()
 i2c=pi.I2C()
@@ -12,6 +12,12 @@ acc.setup()
 
 while True:
     (acc_x,acc_y,acc_z)=acc.get_acc_value()
+    (temp)=acc.get_temp_value()
+    (gyro_x,gyro_y,gyro_z)=acc.get_gyro_value()
 
-    print(acc_x,acc_y,acc_z)
+    print("ACC_X:",acc_x,"ACC_Y:",acc_y,"ACC_Z:",acc_z,"\n")
+    print("TEMP:",temp,"\n")
+    print("GYRO_X:",gyro_x,"GYRO_Y:".gyro_y,"GYRO_Z:",gyro_z,"\n")
+    print("\n")
+
     time.sleep(1/SAMPLING_RATE)
