45fa15b1d482ba66c1e979cf56d44953b4ccb35d
KENT(けんT) <52515974+4456-kent@users.noreply.github.com>
2019-12-04 20:09:41 +0900

    Update test_MPU6050.py
    謎の競合の後、かなり前のコードが一部残っていたので改めた

--- プログラムのソースコード/MPU6050/test_MPU6050.py
+++ プログラムのソースコード/MPU6050/test_MPU6050.py
@@ -3,15 +3,15 @@ import time
 import mpu6050
 
 mpu6050_addr=0x68
+SAMPLING_RATE=10
 
 pi.wiringPiSetupGpio()
 i2c=pi.I2C()
 acc=mpu6050.mpu6050(i2c,mpu6050_addr)
-acc.set_accel_range()
-acc.set_gyro_range()
+acc=mpu6050.setuo()
 
 while True:
-    (acc_x,acc_y,acc_z,gyro_x,gyro_y,gyro_z)=acceleration.get_value()
+    (acc_x,acc_y,acc_z,gyro_x,gyro_y,gyro_z)=acc.get_value()
 
     print(acc_x,acc_y,acc_z,gyro_x,gyro_y,gyro_z)
     time.sleep(1/SAMPLING_RATE)
