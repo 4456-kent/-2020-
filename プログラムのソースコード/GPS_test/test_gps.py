e67a0e361b0885cb296b314d17b0cc900b1d2a0f
KENT(けんT) <52515974+4456-kent@users.noreply.github.com>
2019-12-16 08:43:53 +0900

    Create test_gps.py
    コンソール上にGPS生データ(NMEAフォーマット文)を表示させるテストコード

---
+++ プログラムのソースコード/GPS_test/test_gps.py
@@ -0,0 +1,17 @@
+import serial
+
+s=gps.Serial('/dev/tty50',9600,timeout=1)   #ボーレート9600bps
+"""
+timeoutはシリアルポートをオープンするときのパラメータでreadメソッドのタイムアウト時間を指定することができる
+timeout=None    受信完了まで待つ
+timeout=0   直ちに処理が返る
+timeout=x   x秒後にタイムアウト
+"""
+
+for in range(10):
+    gps_data=gps.readline()
+    print(gps_data)
+
+"""
+ここでコンソール上に現れる文字列はNMEAフォーマットという形式でGPSの生データである
+"""
