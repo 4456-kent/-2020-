2b83d97862273dd93a6b6df3d8e1d593f9c5f8e2
KENT(けんT) <52515974+4456-kent@users.noreply.github.com>
2020-02-11 20:32:06 +0900

    Update test_camera(2).py

--- プログラムのソースコード/Camera/test_camera(2).py
+++ プログラムのソースコード/Camera/test_camera(2).py
@@ -1,9 +1,10 @@
 import time
 import picamera
 
-while(i<100):
+while(i<10):
     with picamera.PiCamera() as camera:
         camera.resolution=(1024,768)
         time.sleep(0.1)
-        camera.capture('test_photo%d.jpg'%i+1)
+        camera.capture('test_photo%03d.jpg'%i+1)
     time.sleep(0.1)
+    i+=1
