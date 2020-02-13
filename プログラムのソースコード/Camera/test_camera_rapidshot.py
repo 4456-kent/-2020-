813d056656790dd5d0147079ac661a033dbd22a9
KENT(けんT) <52515974+4456-kent@users.noreply.github.com>
2020-02-13 18:13:58 +0900

    Update test_camera_rapidshot.py
    画質はともかく，連写してかつ連番でファイルを保存できるようになった．

--- プログラムのソースコード/Camera/test_camera_rapidshot.py
+++ プログラムのソースコード/Camera/test_camera_rapidshot.py
@@ -1,10 +1,10 @@
 import time
 import picamera
 
-while(i<10):
+for i in range(0,10):
     with picamera.PiCamera() as camera:
         camera.resolution=(1024,768)
         time.sleep(0.1)
-        camera.capture('test_photo%03d.jpg'%i+1)
-    time.sleep(0.1)
-    i+=1
+
+        file_name='test_photo'+str(i+1)+'.jpg'
+        camera.capture(file_name)
