fca68d68a5925fa01f8d8761d843eee76f2657a5
KENT(けんT) <52515974+4456-kent@users.noreply.github.com>
2020-02-13 16:38:06 +0900

    Update mpu6050.py
    加速度・角速度でレンジ外の値が返ってくるのを修正
    原因は，負の加速度，角速度になった時，(取得した値)-(フルレンジ)*2の補正が必要だった
    以上でmpu6050.pyの修正を終了とする．

--- プログラムのソースコード/MPU6050/mpu6050.py
+++ プログラムのソースコード/MPU6050/mpu6050.py
@@ -7,6 +7,7 @@
 """
 
 import wiringpi as pi
+import time
 
 class mpu6050:
  
@@ -17,15 +18,23 @@ class mpu6050:
         self.mpu6050=self.i2c.setup(self.addr)
 
     def setup(self):    #最初のI2Cの書き込み
-        self.i2c.writeReg8(self.mpu6050,0x6b,0x01)  #Bit6のSLEEPをLOWにしないと値が0しか返ってこなくなる
+        self.i2c.writeReg8(self.mpu6050,0x6b,0x80)  #デバイスリセット
+        time.sleep(0.25)
+        self.i2c.writeReg8(self.mpu6050,0x6b,0x00)  #6bit目を0にしておかないとスリープモードに移行する
+        time.sleep(0.25)
+        self.i2c.writeReg8(self.mpu6050,0x6a,0x07)  
+        time.sleep(0.25)
+        self.i2c.writeReg8(self.mpu6050,0x6a,0x00)
+        time.sleep(0.25)
         self.i2c.writeReg8(self.mpu6050,0x1c,0x08)  #Full Scale Rangeを±4Gに設定
         self.i2c.writeReg8(self.mpu6050,0x1b,0x08)  #Full Scale Rangeを±500deg/sに設定
-        self.i2c.writeReg8(self.mpu6050,0x1a,0x02)  #フィルターを約96Hzに設定
+        self.i2c.writeReg8(self.mpu6050,0x1a,0x00)  #config
 
 
     def get_acc_value(self):    #加速度の値取得
         buf=[]
         ACCEL_SCALE_MODIFIER=8192.0 #Full Scale Rangeを±4Gに設定したときの値
+        ACCEL_RANGE=4.0
         i=0
         while(i<6):
              buf.append(self.i2c.readReg8(self.mpu6050,0x3b+i))
@@ -35,10 +44,21 @@ class mpu6050:
         acc_y_mesure=((buf[2] << 8) | buf[3])
         acc_z_mesure=((buf[4] << 8) | buf[5])
 
-        acc_x=acc_x_mesure/ACCEL_SCALE_MODIFIER
-        acc_y=acc_y_mesure/ACCEL_SCALE_MODIFIER
-        acc_z=acc_z_mesure/ACCEL_SCALE_MODIFIER
-        
+        if(acc_x_mesure<=ACCEL_SCALE_MODIFIER*ACCEL_RANGE):
+            acc_x=acc_x_mesure/ACCEL_SCALE_MODIFIER
+        elif(acc_x_mesure>ACCEL_SCALE_MODIFIER*ACCEL_RANGE):
+            acc_x=acc_x_mesure/ACCEL_SCALE_MODIFIER-ACCEL_RANGE*2
+            
+        if(acc_y_mesure<=ACCEL_SCALE_MODIFIER*ACCEL_RANGE):
+            acc_y=acc_y_mesure/ACCEL_SCALE_MODIFIER
+        elif(acc_y_mesure>ACCEL_SCALE_MODIFIER*ACCEL_RANGE):
+            acc_y=acc_y_mesure/ACCEL_SCALE_MODIFIER-ACCEL_RANGE*2
+
+        if(acc_z_mesure<=ACCEL_SCALE_MODIFIER*ACCEL_RANGE):
+            acc_z=acc_z_mesure/ACCEL_SCALE_MODIFIER
+        elif(acc_z_mesure>ACCEL_SCALE_MODIFIER*ACCEL_RANGE):
+            acc_z=acc_z_mesure/ACCEL_SCALE_MODIFIER-ACCEL_RANGE*2
+            
         return(acc_x,acc_y,acc_z)
 
     def get_temp_value(self):   #気温の値を取得
@@ -56,7 +76,8 @@ class mpu6050:
         
     def get_gyro_value(self):   #角速度の値を取得
         buf=[]
-        GYRO_SCALE_MODIFIRE=65.5    ##Full Scale Rangeを±500deg/sに設定したときの値
+        GYRO_SCALE_MODIFIRE=65.5    #Full Scale Rangeを±500deg/sに設定したときの値
+        GYRO_RANGE=500
         i=0
         while(i<6):
              buf.append(self.i2c.readReg8(self.mpu6050,0x43+i))
@@ -66,9 +87,20 @@ class mpu6050:
         gyro_y_mesure=((buf[2] << 8) | buf[3])
         gyro_z_mesure=((buf[4] << 8) | buf[5])
 
-        gyro_x=gyro_x_mesure/GYRO_SCALE_MODIFIRE
-        gyro_y=gyro_y_mesure/GYRO_SCALE_MODIFIRE
-        gyro_z=gyro_z_mesure/GYRO_SCALE_MODIFIRE
+        if(gyro_x_mesure<=GYRO_SCALE_MODIFIRE*GYRO_RANGE):      #加速度・角速度ともに0未満になるときはフルレンジの2倍した値を引かなければならない
+            gyro_x=gyro_x_mesure/GYRO_SCALE_MODIFIRE
+        elif(gyro_x_mesure>GYRO_SCALE_MODIFIRE*GYRO_RANGE):
+            gyro_x=gyro_x_mesure/GYRO_SCALE_MODIFIRE-GYRO_RANGE*2
+
+        if(gyro_y_mesure<=GYRO_SCALE_MODIFIRE*GYRO_RANGE):
+            gyro_y=gyro_y_mesure/GYRO_SCALE_MODIFIRE
+        elif(gyro_y_mesure>GYRO_SCALE_MODIFIRE*GYRO_RANGE):
+            gyro_y=gyro_y_mesure/GYRO_SCALE_MODIFIRE-GYRO_RANGE*2
+
+        if(gyro_z_mesure<=GYRO_SCALE_MODIFIRE*GYRO_RANGE):
+            gyro_z=gyro_z_mesure/GYRO_SCALE_MODIFIRE
+        elif(gyro_z_mesure>GYRO_SCALE_MODIFIRE*GYRO_RANGE):
+            gyro_z=gyro_z_mesure/GYRO_SCALE_MODIFIRE-GYRO_RANGE*2
 
         return(gyro_x,gyro_y,gyro_z)
 
